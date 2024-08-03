4642cae17cbb553967a512bb6a237b1c62e7683a
seon <tjswlghks0722@naver.com>
2024-08-03 22:42:54 +0900

    Initial commit

---
+++ src/main/java/com/seon/planty/PlantyApplication.java
@@ -0,0 +1,13 @@
+package com.seon.planty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seon/planty/PlantyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seon.planty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
